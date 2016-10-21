7b8d96bc0177f5a1532dc6ce4ff70d62e43d0749
Robertslob <rjrslob@gmail.com>
2016-10-21 11:50:54 +0200

    Parser x, y not case dependent

--- ShapeDrawing/ShapeDrawing/Parser.cs
+++ ShapeDrawing/ShapeDrawing/Parser.cs
@@ -16,33 +16,27 @@ public class Parser
 		foreach(XmlNode shape in doc.SelectNodes("/shapes/*"))
 		{
 			string type = shape.Name;
-			int x; int y; int width; int height;
-			switch(type)
+            int width, height;
+            int x = int.Parse(shape.Attributes["x"].Value);
+            int y = int.Parse(shape.Attributes["y"].Value);
+            switch (type)
             {
-
                 case "rectangle":
-					x = int.Parse(shape.Attributes["x"].Value);
-					y = int.Parse(shape.Attributes["y"].Value);
 					width = int.Parse(shape.Attributes["width"].Value);
 					height = int.Parse(shape.Attributes["height"].Value);
                     shapes.Add(new Rectangle(x, y, width, height));
                     break;
                 case "circle":
-					x = int.Parse(shape.Attributes["x"].Value);
-					y = int.Parse(shape.Attributes["y"].Value);
 					int size = int.Parse(shape.Attributes["size"].Value);
                     shapes.Add(new Circle(x, y, size));
                     break;
 				case "star":
-					x = int.Parse(shape.Attributes["x"].Value);
-					y = int.Parse(shape.Attributes["y"].Value);
 					width = int.Parse(shape.Attributes["width"].Value);
 					height = int.Parse(shape.Attributes["height"].Value);
-					shapes.Add (new Star(x,y,width,height));
-					break;
+					shapes.Add (new Star(x, y, width, height));
+					break; 
             }
-		}
-		
+		}		
 		return shapes;
 	}
 }
